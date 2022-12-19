496a0d2a3a5268a94daedade23fc67024ed42474
Chase Million <1483210+cmillion@users.noreply.github.com>
2022-12-18 22:37:20 -0500

    checkpoint backup

--- src/get_data.py
+++ src/get_data.py
@@ -9,7 +9,7 @@ for e in eclipses:
     edir = f"e{str(e).zfill(5)}"
     if not os.path.exists(f"../data/lightcurves/{edir}/"):
         os.makedirs(f"../data/lightcurves/{edir}/")
-        cmd = f"aws s3 sync s3://dream-pool/{edir} ../data/lightcurves/{edir}/ --exclude '*fits*' --exclude '*gif*' --exclude '*parquet*' --exclude '*full-photom*'"
-    cmd = f"aws s3 sync s3://dream-pool/{edir}/ ../data/lightcurves/{edir}/ --exclude '*csv*' --exclude '*parquet*' --exclude '*30s*' --exclude '*jpg*'"
+    cmd = f"aws s3 sync s3://dream-pool/{edir} ../data/lightcurves/{edir}/ --exclude '*30s.fits*' --exclude '*gif*' --exclude '*parquet*' --exclude '*full-photom*'"
+    #cmd = f"aws s3 sync s3://dream-pool/{edir}/ ../data/lightcurves/{edir}/ --exclude '*csv*' --exclude '*parquet*' --exclude '*30s*' --exclude '*jpg*'"
     print(cmd)
     os.system(cmd)
